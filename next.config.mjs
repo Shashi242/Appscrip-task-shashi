de0aa96a3ff10138d7e80014d6e0eff8ba2a309a
Shashi Raj <raj34828@gmail.com>
2025-04-10 11:43:23 +0530

    Ready to overwrite remote with local project

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-const { withNetlify } = require('@netlify/next');
-const nextConfig = withNetlify({
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['fakestoreapi.com'],
   },
-});
+};
 
 export default nextConfig;
